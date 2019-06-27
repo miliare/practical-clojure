7297e80f7d5d311f8d4d04db35c2573fdeb5bcd3
Rémy Milia <milia.remy@gmail.com>
2019-06-27 20:36:59 +0200

    make a first test pass, filter monuments by region

--- test/monumental/core_test.clj
+++ test/monumental/core_test.clj
@@ -2,9 +2,10 @@
   (:require [midje.sweet :refer :all]
             [monumental.core :refer :all]))
 
-(println "You should expect to see three failures below.")
 
-(facts "about `first-element`"
-  (fact "it normally returns the first element"
-    (first-element [1 2 3] :default) => 1
-    (first-element '(1 2 3) :default) => 1))
+(facts "about `monuments-by-region`"
+       (fact "it should return monuments for matching regions"
+             (let [monuments '({:REG "Picardie"})
+                   region "Picardie"]
+               (monuments-by-region monuments region) => monuments)))
+
